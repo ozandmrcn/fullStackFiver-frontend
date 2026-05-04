c3b725353f4668e92a311b3dbbd2b1d88474240e
ozandmrcn <ozandmrcn47@gmail.com>
2026-05-04 09:56:43 +0300

    feat: add Vercel Speed Insights and initialize React application with providers

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ToastContainer } from "react-toastify";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const queryClient = new QueryClient();
 
@@ -14,5 +15,6 @@ createRoot(document.getElementById("root")!).render(
       <ToastContainer />
     </QueryClientProvider>
     <Analytics />
+    <SpeedInsights />
   </>,
 );
